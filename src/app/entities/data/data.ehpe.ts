f5e9d9c8e97e928eb003cd2a703a8be7d6a97245
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-06-28 22:12:53 +0200

    feature: add gps altitude and ehpe data

---
+++ src/app/entities/data/data.ehpe.ts
@@ -0,0 +1,5 @@
+import {Data} from './data';
+
+export class DataEHPE extends Data {
+  protected unit = 'meters';
+}
---
+++ src/app/entities/data/data.gps-altitude.ts
@@ -0,0 +1,4 @@
+import {DataAltitude} from './data.altitude';
+
+export class DataGPSAltitude extends DataAltitude {
+}
