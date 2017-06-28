d8f5983ee9134a4dc007b6aa86708b20acca4cd8
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-06-28 16:02:01 +0200

    chore: add 2 new datatypes of respiration rate and absolute pressure

---
+++ src/app/entities/data/data.absolute-pressure.ts
@@ -0,0 +1,5 @@
+import {Data} from './data';
+
+export class DataAbsolutePressure extends Data {
+  protected unit = 'hpa';
+}
---
+++ src/app/entities/data/data.respiration-rate.ts
@@ -0,0 +1,5 @@
+import {Data} from './data';
+
+export class DataRespirationRate extends Data {
+  protected unit = '';
+}
