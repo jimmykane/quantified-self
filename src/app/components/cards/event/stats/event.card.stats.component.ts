ded0dd887fa63db47438cb4effc91b0450f49900
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-07-06 16:29:11 +0200

    chore: add stats per event

--- src/app/components/cards/event/stats/event.card.stats.component.ts
+++ src/app/components/cards/event/stats/event.card.stats.component.ts
@@ -1,5 +1,9 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges} from '@angular/core';
 import {EventInterface} from '../../../../entities/events/event.interface';
+import {DataHeartRate} from '../../../../entities/data/data.heart-rate';
+import {DataCadence} from '../../../../entities/data/data.cadence';
+import {DataPower} from '../../../../entities/data/data.power';
+import {DataTemperature} from "../../../../entities/data/data.temperature";
 
 
 @Component({
@@ -9,6 +13,39 @@ import {EventInterface} from '../../../../entities/events/event.interface';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class EventCardStatsComponent {
+export class EventCardStatsComponent implements OnChanges {
   @Input() event: EventInterface;
+
+  public dataTypeAverages = [
+    {
+      name: DataHeartRate.name,
+      value: null,
+      iconName: 'heartbeat',
+      units: 'BPM'
+    },
+    {
+      name: DataCadence.name,
+      value: null,
+      iconName: 'circle-o-notch',
+      units: 'SPM'
+    },
+    {
+      name: DataTemperature.name,
+      value: null,
+      iconName: 'thermometer',
+      units: 'Celsius'
+    },
+    {
+      name: DataPower.name,
+      value: null,
+      iconName: 'flash',
+      units: 'WATTS'
+    }
+  ];
+
+  ngOnChanges() {
+    this.dataTypeAverages.forEach((dataTypeAverage) => {
+      dataTypeAverage.value = this.event.getDataTypeAverage(dataTypeAverage.name);
+    });
+  }
 }
