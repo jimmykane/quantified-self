9dd36142659404c8fea5e3460c4b054e5c7682ac
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-07-07 14:32:03 +0200

    Revert "chore: add table module"
    This reverts commit 828ddc16d20ba0da08ef046c2b6c55a8e56b2075.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,6 @@ import {HttpModule} from '@angular/http';
 import {EventCardMapLocationComponent} from './components/cards/event/map/location/event.card.map.location.component';
 import {EventCardMapWeatherComponent} from './components/cards/event/map/weather/event.card.map.weather.component';
 import {Angular2FontawesomeModule} from 'angular2-fontawesome';
-import {MdTableModule} from "@angular/material/typings/table";
 
 @NgModule({
   imports: [
@@ -54,7 +53,6 @@ import {MdTableModule} from "@angular/material/typings/table";
     MdSidenavModule,
     MdToolbarModule,
     MdGridListModule,
-    MdTableModule,
     MomentModule,
     AmChartsModule,
     Angular2FontawesomeModule,
