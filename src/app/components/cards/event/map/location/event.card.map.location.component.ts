52d16579d62d64b97fa4271a93a0ffb3ba8f7607
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-07-14 21:41:28 +0200

    chore: check array

--- src/app/components/cards/event/map/location/event.card.map.location.component.ts
+++ src/app/components/cards/event/map/location/event.card.map.location.component.ts
@@ -35,7 +35,7 @@ export class EventCardMapLocationComponent implements OnChanges {
   }
 
   private processReverseGeocodeResults = (results, status) => {
-    if (!status === google.maps.GeocoderStatus.OK || !results) {
+    if (!status === google.maps.GeocoderStatus.OK || !results[0]) {
       return;
     }
     results[0].address_components.forEach((addressComponent) => {
