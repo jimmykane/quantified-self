78d67b4373bb8dbe1661b527568d83c9c21d9f42
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-09-13 21:31:02 +0200

    chore: fix weather item

--- src/app/services/weather/app.weather-underground.weather.service.ts
+++ src/app/services/weather/app.weather-underground.weather.service.ts
@@ -10,7 +10,7 @@ import {WeatherServiceInterface} from "app/services/weather/app.weather.service.
 
 
 @Injectable()
-export class WeatherUndergroundWeatherService implements WeatherServiceInterface{
+export class WeatherUndergroundWeatherService implements WeatherServiceInterface {
 
   private historyApiUrl = 'https://api.wunderground.com/api/{apiKey}/history_{YYYYMMDD}/q/{lat},{lon}.json';
   private apiKey = 'a6dbe6951244fa18';
@@ -26,21 +26,28 @@ export class WeatherUndergroundWeatherService implements WeatherServiceInterface
         .replace('{YYYYMMDD}', event.getFirstActivity().getStartDate().toISOString().slice(0, 10).replace(/-/g, ''))
         .replace('{apiKey}', this.apiKey))
       .map((response) => {
-        return new Weather([...JSON.parse(response.text())
-          .history.observations.reduce((weatherItems: Map<string, WeatherItem>, observation: any) => {
-            if (Number(observation.date.hour) >= event.getFirstActivity().getStartDate().getHours() &&
-              Number(observation.date.hour) <= event.getLastActivity().getEndDate().getHours() &&
-              Number(observation.tempm) !== -9999) {
-              const weatherItemDate = new Date(event.getFirstActivity().getStartDate().getTime());
-              weatherItemDate.setHours(Number(observation.date.hour));
-              weatherItems.set(weatherItemDate.toISOString(), new WeatherItem(
-                weatherItemDate,
-                observation.conds,
-                Number(observation.tempm))
-              )
-            }
-            return weatherItems;
-          }, new Map<string, WeatherItem>()).values()]);
+        const weatherItemsMap: Map<string, WeatherItem> = JSON.parse(response.text())
+          .history
+          .observations
+          .reduce(
+            (weatherItems: Map<string, WeatherItem>, observation: any) => {
+              if (Number(observation.date.hour) >= event.getFirstActivity().getStartDate().getHours() &&
+                Number(observation.date.hour) <= event.getLastActivity().getEndDate().getHours() &&
+                Number(observation.tempm) !== -9999) {
+                const weatherItemDate = new Date(event.getFirstActivity().getStartDate().getTime());
+                weatherItemDate.setHours(Number(observation.date.hour));
+                weatherItems.set(weatherItemDate.toISOString(), new WeatherItem(
+                  weatherItemDate,
+                  observation.conds,
+                  Number(observation.tempm))
+                )
+              }
+              return weatherItems;
+            }, new Map<string, WeatherItem>()
+          );
+        return new Weather(
+          Array.from(weatherItemsMap.values())
+        );
       })
   }
 }
