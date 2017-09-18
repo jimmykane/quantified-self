b9f94f8a9786a682adbb867a24ea83f1367ef0c3
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2017-09-18 14:39:30 +0200

    chore: add implementation for event summary

--- src/app/entities/events/summary/event.summary.ts
+++ src/app/entities/events/summary/event.summary.ts
@@ -4,35 +4,47 @@ import {Weather} from '../../weather/app.weather';
 
 export class EventSummary implements EventSummaryInterface {
 
+  private elapsedTimeInSeconds: number;
+  private totalDistanceInMeters: number;
+  private geoLocationInfo: GeoLocationInfo;
+  private weather: Weather;
+
   setElapsedTimeInSeconds(elapsedTimeInSeconds: number) {
+    this.elapsedTimeInSeconds = elapsedTimeInSeconds;
   }
 
   getElapsedTimeInSeconds(): number {
-    return null;
+    return this.elapsedTimeInSeconds;
   }
 
   setTotalDistanceInMeters(totalDistanceInMeters: number) {
+    this.totalDistanceInMeters = totalDistanceInMeters;
   }
 
   getTotalDistanceInMeters(): number {
-    return null;
+    return this.totalDistanceInMeters;
   }
 
   setGeoLocationInfo(geoLocationInfo: GeoLocationInfo) {
+    this.geoLocationInfo = geoLocationInfo;
   }
 
   getGeoLocationInfo(): GeoLocationInfo {
-    return null;
+    return this.geoLocationInfo;
   }
 
   setWeather(weather: Weather) {
+    this.weather = weather;
   }
 
   getWeather(): Weather {
-    return null;
+    return this.weather;
   }
 
   toJSON(): any {
-    return null;
+    return {
+      elapsedTimeInSecods: this.elapsedTimeInSeconds,
+      totalDistanceInMeters: this.totalDistanceInMeters,
+    };
   }
 }
