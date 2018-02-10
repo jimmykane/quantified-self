c24494a9382661e8cac3150eb7b03dee0a938b0c
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-02-10 22:01:01 +0100

    feature: clear storage on verion

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 import {Log} from 'ng2-logger';
 
 if (environment.production) {
   enableProdMode();
   Log.setProductionMode();
+
+}
+
+if (localStorage.getItem('version') !== 'v0.0.1') {
+  localStorage.clear();
+  localStorage.setItem('version', 'v0.0.1');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
