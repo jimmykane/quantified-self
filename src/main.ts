be7073e7895ff2cff061ab8e1290d62d8667d3fd
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-06 22:05:19 +0100

    chore: move analytics to production only

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,16 @@ declare function require(moduleName: string): any;
 const { version: appVersion } = require('../package.json');
 
 if (environment.production) {
+  // add Google Analytics script to <head>
+  const analyticsScript = document.createElement('script');
+  analyticsScript.innerHTML = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-61188889-2', 'auto');
+    ga('send', 'pageview');`;
+  document.head.appendChild(analyticsScript);
+
   enableProdMode();
   Log.setProductionMode();
 }
