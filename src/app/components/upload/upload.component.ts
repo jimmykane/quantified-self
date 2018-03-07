20a1c383eb3d1d7c06d36745a0120064e4ab3c57
bellos <bellos@intralot.com>
2018-03-07 15:32:56 +0200

    chore: Refactored upload component. JSON is currently the only supported format

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,87 +1,67 @@
 import {Component} from '@angular/core';
 import {EventService} from '../../services/app.event.service';
 import {Router} from '@angular/router';
-import {LocalStorageService} from '../../services/storage/app.local.storage.service';
-import {EventInterface} from '../../entities/events/event.interface';
 
 @Component({
-  selector: 'app-upload',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css'],
+    selector: 'app-upload',
+    templateUrl: './upload.component.html',
+    styleUrls: ['./upload.component.css'],
 })
 
 export class UploadComponent {
 
-  //whether an upload is currently active
-  isUploadActive: boolean;
+    //whether an upload is currently active
+    isUploadActive: boolean = false;
 
-  constructor(private eventService: EventService, private localStorageService: LocalStorageService, private router: Router) {
-    this.isUploadActive = false;
-  }
-
-  openFile(event): void {
-    this.isUploadActive = true;
-    const input = event.target;
-    for (let index = 0; index < input.files.length; index++) {
-      const fileReader = new FileReader;
-      fileReader.onload = () => {
-        const name = input.files[index].name.split('.')[0];
-        if (['tcx', 'gpx'].indexOf(input.files[index].name.split('.').pop()) > -1) {
-          this.eventService
-            .createEventFromXMLString(fileReader.result)
-            .then((newEvent: EventInterface) => {
-              newEvent.setName(name);
-              this.eventService.generateEventSummaries(newEvent).then((newEventWithSummaries: EventInterface) => {
-                this.eventService.saveEvent(newEventWithSummaries);
-                this.isUploadActive = false;
-                this.router.navigate(['dashboard']);
-              });
-            })
-            .catch((error) => {
-              this.isUploadActive = false;
-              console.error('Could not load event from file' + input.files[index].name, error);
-            });
-        } else if (input.files[index].name.split('.').pop() === 'fit') {
-          this.readAsBinary(input.files[index]);
-        } else if (input.files[index].name.split('.').pop() === 'json') {
-          this.eventService
-            .createEventFromSuuntoJSONString(fileReader.result)
-            .then((newEvent: EventInterface) => {
-              newEvent.setName(name);
-              this.eventService.generateGeoAndWeather(newEvent).then(() => {
-                this.eventService.saveEvent(newEvent);
-                this.isUploadActive = false;
-                this.router.navigate(['dashboard']);
-              });
-            })
-            .catch((error) => {
-              this.isUploadActive = false;
-              console.error('Could not load event from file' + input.files[index].name, error);
-            });
-        }
-      };
-      // Read it
-      fileReader.readAsText(input.files[index]);
+    constructor(private eventService: EventService, private router: Router) {
     }
-  }
 
-  // @todo refactor
-  private readAsBinary(file: File) {
-    const fileReader = new FileReader;
-    fileReader.onloadend = (ev: ProgressEvent) => {
-      this.eventService
-        .createEventFromJSONFITString(fileReader.result)
-        .then((newEvent: EventInterface) => {
-          newEvent.setName(file.name.split('.')[0]);
-          this.eventService.generateEventSummaries(newEvent).then((newEventWithSummaries: EventInterface) => {
-            this.eventService.saveEvent(newEventWithSummaries);
-          });
-        })
-        .catch((error) => {
-          console.error('Could not load event from file' + file.name, error);
+    /**
+     * Process each uploaded activity
+     * @param file
+     * @returns {Promise}
+     */
+    processFile(file):Promise{
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader;
+            fileReader.onload = async () => {
+                const {name} = file,
+                      extension = name.split('.').pop(),
+                      activityName = name.split('.')[0];
+                if (extension === 'json') {
+                    let newEvent;
+                    try {
+                        newEvent = await this.eventService.createEventFromSuuntoJSONString(fileReader.result);
+                    }catch(error) {
+                        console.error('Could not load event from file' + file.name, error);
+                        reject(error);
+                        return;
+                    }
+                    newEvent.setName(activityName);
+                    await this.eventService.generateGeoAndWeather(newEvent)
+                    this.eventService.saveEvent(newEvent);
+                    resolve();
+                }
+            };
+            // Read it
+            fileReader.readAsText(file);
         });
-    };
-    fileReader.readAsArrayBuffer(file);
+    }
 
-  }
+    async openFile(event): void {
+        this.isUploadActive = true;
+        const input = event.target;
+        const processPromises = [];
+        for (let index = 0; index < input.files.length; index++) {
+            processPromises.push(this.processFile(input.files[index]));
+        }
+        try{
+          await Promise.all(processPromises);
+          this.router.navigate(['dashboard']);
+        }catch(error){
+            console.error('Some of the files could not be processed', error);
+        }finally{
+            this.isUploadActive = false;
+        }
+    }
 }
