761d39048af8b4118ba19d795714b1b2cf132d18
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-11 12:45:07 +0100

    chore: add getAsHR and docblock

--- src/app/entities/data/ibi/data.ibi.ts
+++ src/app/entities/data/ibi/data.ibi.ts
@@ -2,7 +2,12 @@ import {SerializableClassInterface} from '../../serializable/serializable.class.
 
 export class IBIData implements SerializableClassInterface {
 
-  private ibiDataMap: Map<number, number> = new Map();
+  /**
+   * Key is time elapsed since start of the array
+   * value is the interval
+   * @type {Map<number, number>}
+   */
+  private ibiDataMap: Map<number, number> = new Map<number, number>();
 
   constructor(ibiDataArray?: Array<number>) {
     if (ibiDataArray) {
@@ -16,6 +21,18 @@ export class IBIData implements SerializableClassInterface {
 
   public getIBIDataArray(): Array<number> {
     return Array.from(this.ibiDataMap.values());
+  }
+
+  public getIBIData(): Map<number, number> {
+    return this.ibiDataMap;
+  }
+
+  public getAsHR(): Map<number, number> {
+    const hrDataMap = new Map();
+    this.ibiDataMap.forEach((value, key, map) => {
+      hrDataMap.set(key, Math.round(60000 / value))
+    });
+    return hrDataMap;
   }
 
   toJSON(): any {
