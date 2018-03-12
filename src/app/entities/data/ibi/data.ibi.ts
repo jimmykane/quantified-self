becd1c7eb48f485eb2812ac65e67ad564b9f4517
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-12 18:48:48 +0100

    chore: enhancements

--- src/app/entities/data/ibi/data.ibi.ts
+++ src/app/entities/data/ibi/data.ibi.ts
@@ -4,32 +4,54 @@ import {IBIFilters} from './data.ibi.filters';
 export class IBIData implements SerializableClassInterface {
 
   /**
-   * Key is time elapsed since start of the array
-   * value is the interval
+   * Key is time time since start of the array
+   * value is the ibi
    * @type {Map<number, number>}
    */
   private ibiDataMap: Map<number, number> = new Map<number, number>();
 
   constructor(ibiDataArray?: Array<number>) {
     if (ibiDataArray) {
-      ibiDataArray.reduce((totalTime, ibiData) => {
-        if (ibiData > 0) {
-          totalTime += ibiData;
-          this.ibiDataMap.set(totalTime, ibiData);
-        }
-        return totalTime;
-      }, 0)
+      this.parseIBIArray(ibiDataArray);
     }
   }
 
-  public setIBI(elapsedTime, ibi) {
-    this.ibiDataMap.set(elapsedTime, ibi)
+  /**
+   * Parses an IBI data array
+   * eg: [600, 600, 100] becomes a map of {600:600, 1200: 600, 1300:100}
+   * @param {Array<number>} ibiArray
+   */
+  public parseIBIArray(ibiArray: Array<number>) {
+    ibiArray.reduce((totalTime, ibiData) => {
+      if (ibiData > 0) {
+        totalTime += ibiData;
+        this.ibiDataMap.set(totalTime, ibiData);
+      }
+      return totalTime;
+    }, 0)
   }
 
+  /**
+   * Sets the ibi for the specific time
+   * @param time
+   * @param ibi
+   */
+  public setIBI(time, ibi) {
+    this.ibiDataMap.set(time, ibi)
+  }
+
+  /**
+   * Gets the IBI data map
+   * @return {Map<number, number>}
+   */
   public getIBIData(): Map<number, number> {
     return this.ibiDataMap;
   }
 
+  /**
+   * Gets the IBI data map but uses BPM units instead of IBI
+   * @return {Map<number, number>}
+   */
   public getAsBPM(): Map<number, number> {
     const hrDataMap = new Map();
     this.ibiDataMap.forEach((value, key, map) => {
