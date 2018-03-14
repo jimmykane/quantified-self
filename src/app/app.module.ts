f97005f640cefdf06cfc4c7d2b183ff1824267ed
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-14 13:53:59 +0100

    chore: add input module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import {
   MatGridListModule, MatIconModule,
   MatMenuModule, MatProgressBarModule,
   MatSidenavModule, MatTableModule,
-  MatTabsModule, MatToolbarModule, MatCheckboxModule, MatSliderModule, MatSnackBarModule
+  MatTabsModule, MatToolbarModule, MatCheckboxModule, MatSliderModule, MatSnackBarModule, MatInputModule
 } from '@angular/material';
 import 'hammerjs';
 import {EventCardComponent} from './components/cards/event/event.card.component';
@@ -76,6 +76,7 @@ import {EventCardToolsComponent} from './components/cards/event/tools/event.card
     MatCheckboxModule,
     MatSliderModule,
     MatSnackBarModule,
+    MatInputModule,
     FormsModule,
     Angular2FontawesomeModule,
     MatProgressBarModule
