41e02ffb35672aab6f1aaae40abb92d425d2da53
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-16 21:45:51 +0100

    chore: add test for data heart rate

---
+++ src/app/entities/data/data.spec.ts
@@ -0,0 +1,19 @@
+import {DataInterface} from './data.interface';
+import {DataHeartRate} from './data.heart-rate';
+
+describe('Data', function () {
+
+  let data: DataInterface;
+
+  beforeEach(() => {
+    data = new DataHeartRate(60);
+  });
+
+  it('should get a value of 60', function () {
+    expect(data.getValue()).toBe(60);
+  });
+
+  it('should get the unit of bpm', function () {
+    expect(data.getUnit()).toBe('bpm');
+  });
+});
