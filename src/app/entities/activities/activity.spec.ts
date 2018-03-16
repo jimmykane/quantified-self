c6e1928ce44f947bc7e1aad8b57356723cb536d8
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-16 23:13:49 +0100

    chore: add activity spec

---
+++ src/app/entities/activities/activity.spec.ts
@@ -0,0 +1,21 @@
+import {ActivityInterface} from "./activity.interface";
+import {Activity} from "./activity";
+
+describe('Activity', function () {
+
+  // let activity: ActivityInterface;
+  //
+  // beforeEach(() => {
+  //   activity = new Activity();
+  // });
+  //
+  // it('should export correctly to JSON', function () {
+  //   expect(creator.toJSON()).toEqual({
+  //     name: 'name',
+  //     serialNumber: 'SerialNumber',
+  //     swInfo: 'SWInfo',
+  //     hwInfo: 'HWInfo',
+  //   });
+  //
+  // });
+});
