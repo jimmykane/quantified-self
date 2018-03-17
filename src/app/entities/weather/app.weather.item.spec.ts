9e9b19ed3936c19a28f6ff3c285b757dc2b55b35
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-17 21:21:20 +0100

    chore: add tests for weather

---
+++ src/app/entities/weather/app.weather.item.spec.ts
@@ -0,0 +1,19 @@
+import {WeatherItem} from './app.weather.item';
+
+describe('WeatherItem', function () {
+
+  let weatherItem: WeatherItem;
+
+  beforeEach(() => {
+    weatherItem = new WeatherItem(new Date(0), 'test', 0)
+  });
+
+  it('should export correctly to JSON', function () {
+    expect(weatherItem.toJSON()).toEqual({
+      'date': '1970-01-01T00:00:00.000Z',
+      'conditions': 'test',
+      'temperatureInCelsius': 0
+    });
+  });
+
+});
