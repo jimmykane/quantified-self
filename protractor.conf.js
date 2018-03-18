fc7167226ff10b117ffb88ac6ab6e4b190d87c9e
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-18 13:53:16 +0100

    Revert "Revert "chore: remove print""
    This reverts commit 3cd2b95e5b86c6a36e53e95569ca0315da4cada8.

--- protractor.conf.js
+++ protractor.conf.js
@@ -22,7 +22,6 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
   },
   onPrepare() {
     require('ts-node').register({
