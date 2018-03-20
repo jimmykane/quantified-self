7496a3b0ab7c24a5153a7433754effe09b5e8e9e
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-20 21:21:41 +0100

    chore: add spec boilerplate

---
+++ src/app/entities/events/adapters/importers/importer.json.spec.ts
@@ -0,0 +1,14 @@
+import {EventImporterJSON} from './importer.json';
+
+describe('EventImporterJSON', () => {
+
+  let eventImporter: EventImporterJSON;
+
+  beforeEach(() => {
+    eventImporter = new EventImporterJSON();
+  });
+
+  it('should get a value of 60', () => {
+  });
+
+});
