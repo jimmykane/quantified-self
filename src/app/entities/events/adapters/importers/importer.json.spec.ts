e1cbdc42a7e5b43b296ecfffc174e0c00fc4d082
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-21 13:25:44 +0100

    chore: improve json decoder tests

--- src/app/entities/events/adapters/importers/importer.json.spec.ts
+++ src/app/entities/events/adapters/importers/importer.json.spec.ts
@@ -1,14 +1,13 @@
 import {EventImporterJSON} from './importer.json';
-
+import {Event} from '../../event';
+const example1  = require('../../../../../../samples/track_examples/json_example.json');
 describe('EventImporterJSON', () => {
 
-  let eventImporter: EventImporterJSON;
-
   beforeEach(() => {
-    eventImporter = new EventImporterJSON();
   });
 
-  it('should get a value of 60', () => {
+  it('should be able to decode an get an event from an example json', () => {
+    expect(EventImporterJSON.getFromJSONString(  JSON.stringify(example1) ) instanceof Event).toBe(true);
   });
 
 });
