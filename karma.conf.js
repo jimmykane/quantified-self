48b72ebfa7114d659a14673ca4a0a005ac1ae485
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-03-22 22:29:18 +0100

    chore: remove phantom and use chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
