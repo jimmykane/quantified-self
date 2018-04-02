0baf8055054fb7b9dd98e607ffc62e5104b40321
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-02 11:58:56 +0200

    chore: add tests

--- src/app/components/upload-info/upload-info.component.spec.ts
+++ src/app/components/upload-info/upload-info.component.spec.ts
@@ -2,9 +2,11 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UploadInfoComponent} from './upload-info.component';
 import {
+  MatIcon,
   MatList, MatListItem, MatProgressBar,
   MatRipple
 } from '@angular/material';
+import {UPLOAD_STATUS} from '../upload/status';
 
 describe('UploadInfoComponent', () => {
   let component: UploadInfoComponent;
@@ -12,7 +14,7 @@ describe('UploadInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [UploadInfoComponent, MatProgressBar, MatList, MatListItem, MatRipple,
+      declarations: [UploadInfoComponent, MatProgressBar, MatList, MatListItem, MatRipple, MatIcon
       ]
     })
       .compileComponents();
@@ -27,4 +29,81 @@ describe('UploadInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should get the correct status icon', () => {
+    expect(component.getActivityStatusIcon({
+      name: 'test',
+      status: UPLOAD_STATUS.PROCESSING
+    })).toBe('autorenew');
+    expect(component.getActivityStatusIcon({
+      name: 'test',
+      status: UPLOAD_STATUS.PROCESSED
+    })).toBe('done');
+    expect(component.getActivityStatusIcon({
+      name: 'test',
+      status: UPLOAD_STATUS.ERROR
+    })).toBe('sync_problem');
+  });
+
+  it('should get the correct amount of processed activities', () => {
+    component.activitiesMetaData = [
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSING
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSING
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSED
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSED
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.ERROR
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.ERROR
+      }
+    ];
+    expect(component.getProcessedActivities().length).toBe(2);
+  });
+
+  it('should get the correct percent of overall progress', () => {
+    component.activitiesMetaData = [
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSING
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSING
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSED
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.PROCESSED
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.ERROR
+      },
+      {
+        name: 'test',
+        status: UPLOAD_STATUS.ERROR
+      }
+    ];
+    expect(component.getOverallProgress()).toBe(33.333333333333336);
+  });
+
 });
