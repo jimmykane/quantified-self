08188a99b261df38088f3ebb83aafc4cc05d5396
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-10 13:52:49 +0200

    chore: fallback to name if no device

--- src/app/entities/events/adapters/importers/suunto/importer.suunto.json.ts
+++ src/app/entities/events/adapters/importers/suunto/importer.suunto.json.ts
@@ -42,7 +42,7 @@ export class EventImporterSuuntoJSON {
 
     // Create a creator and pass it to all activities (later)
     const creator = new Creator();
-    creator.name = ImporterSuuntoDeviceNames[eventJSONObject.DeviceLog.Device.Name];
+    creator.name = ImporterSuuntoDeviceNames[eventJSONObject.DeviceLog.Device.Name] || eventJSONObject.DeviceLog.Device.Name;
     creator.serialNumber = eventJSONObject.DeviceLog.Device.SerialNumber;
     creator.hwInfo = eventJSONObject.DeviceLog.Device.Info.HW;
     creator.swInfo = eventJSONObject.DeviceLog.Device.Info.SW;
