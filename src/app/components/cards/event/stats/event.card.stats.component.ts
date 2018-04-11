1026ac6fcd037b29f059b276fab96be577c62def
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-11 14:38:07 +0200

    chore: add support for material table for activity stats

--- src/app/components/cards/event/stats/event.card.stats.component.ts
+++ src/app/components/cards/event/stats/event.card.stats.component.ts
@@ -1,5 +1,8 @@
 import {ChangeDetectionStrategy, Component, Input, OnChanges} from '@angular/core';
 import {EventInterface} from '../../../../entities/events/event.interface';
+import {MatTableDataSource} from "@angular/material";
+import {SummaryInterface} from "../../../../entities/summary/summary.interface";
+import {Summary} from "../../../../entities/summary/summary";
 
 @Component({
   selector: 'app-event-card-stats',
@@ -8,7 +11,32 @@ import {EventInterface} from '../../../../entities/events/event.interface';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class EventCardStatsComponent {
+export class EventCardStatsComponent implements OnChanges {
   @Input() event: EventInterface;
+  data: MatTableDataSource<Object>;
+  columns: Array<Object>;
+
+  ngOnChanges() {
+    // Create table data
+
+    const data = [];
+    // Create a summary object to get the keys
+    const rows = Object.keys(new Summary())
+    // Filter out the intensityZones
+      .filter(key => key !== 'intensityZones')
+      // Create an array with rows of keys and cols of the event activities stats for the keys
+      .reduce((array, key) => {
+        array.push(
+          this.event.getActivities().reduce((rowObj, activity, index) => {
+            rowObj['Activity ' + (index + 1)] = activity.summary[key];
+            return rowObj;
+          }, {name: key})
+        );
+        return array;
+      }, []);
+
+    this.columns = (Object.keys(rows[0]));
+    this.data = new MatTableDataSource(rows);
+  }
 
 }
