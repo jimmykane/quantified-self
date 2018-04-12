b3812922d7f50df8313b56a4bc882661c06b3d01
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-12 13:16:29 +0200

    chore: add min, max, avg data types

---
+++ src/app/entities/data/data.max.ts
@@ -0,0 +1,5 @@
+import {Data} from './data';
+
+export abstract class DataMax extends Data {
+  static className = 'DataMax';
+}
---
+++ src/app/entities/data/data.min.ts
@@ -0,0 +1,5 @@
+import {Data} from './data';
+
+export abstract class DataMin extends Data {
+  static className = 'DataMin';
+}
