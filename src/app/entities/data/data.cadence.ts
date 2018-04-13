d63d224bc1e02aa81a11e46acc44d39f00bed683
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-14 00:57:21 +0200

    chore: add display value for cadence

--- src/app/entities/data/data.cadence.ts
+++ src/app/entities/data/data.cadence.ts
@@ -4,4 +4,8 @@ export class DataCadence extends Data {
   static className = 'DataCadence';
   static type = 'Cadence';
   static unit = 'spm';
+
+  getDisplayValue() {
+    return Math.round(this.getValue());
+  }
 }
