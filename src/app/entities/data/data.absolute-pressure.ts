eb253ef2649bfa37a767286c65a662b76f2270dd
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-16 10:43:22 +0200

    chore: remove bare min,max,avg and fix abs pressure as number

--- src/app/entities/data/data.absolute-pressure.ts
+++ src/app/entities/data/data.absolute-pressure.ts
@@ -1,6 +1,6 @@
-import {Data} from './data';
+import {DataNumber} from './data.number';
 
-export class DataAbsolutePressure extends Data {
+export class DataAbsolutePressure extends DataNumber {
   static className = 'DataAbsolutePressure';
   static type = 'Absolute Pressure';
   static unit = 'hpa';
