9427ce271bea8cf8d08a27942688e56b17d3908e
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-30 14:22:57 +0200

    chore: add ambit and use the enum

--- src/app/entities/events/adapters/importers/fit/importer.fit.suunto.device.names.ts
+++ src/app/entities/events/adapters/importers/fit/importer.fit.suunto.device.names.ts
@@ -1,4 +1,7 @@
+import {ImporterSuuntoDeviceNames} from "../suunto/importer.suunto.device.names";
+
 export const ImporterFitSuuntoDeviceNames = {
-  29: 'Suunto Spartan Ultra',
-  34: 'Suunto 9',
+  29: ImporterSuuntoDeviceNames.Amsterdam,
+  34: ImporterSuuntoDeviceNames.Ibiza,
+  22: ImporterSuuntoDeviceNames['Suunto Ambit3 Peak'],
 };
