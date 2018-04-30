e037921e8f414baf33813393e53ef0a08bfe3500
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-30 21:19:39 +0200

    chore: fix ascent display

--- src/app/entities/data/data.ascent.ts
+++ src/app/entities/data/data.ascent.ts
@@ -1,6 +1,15 @@
 import {DataDistance} from './data.distance';
+import {Data} from './data';
 
 export class DataAscent extends DataDistance {
   static className = 'DataAscent';
   static type = 'Ascent';
+
+  getDisplayValue() {
+    return this.value;
+  }
+
+  getDisplayUnit(): string {
+    return (<typeof Data>this.constructor).unit;
+  }
 }
