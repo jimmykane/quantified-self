395212c82027ba3adf40756f542fd879a9111c7e
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-04-30 12:47:23 +0200

    chore: do not show hours if they do not exist

--- src/app/entities/data/data.duration.ts
+++ src/app/entities/data/data.duration.ts
@@ -10,6 +10,9 @@ export class DataDuration extends DataNumber {
     const h = Math.floor(d / 3600);
     const m = Math.floor(d % 3600 / 60);
     const s = Math.floor(d % 3600 % 60);
+    if (!h) {
+      return ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
+    }
     return ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2);
   }
 
