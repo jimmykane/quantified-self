cb50c5aec0d0c2cdfeb29b6786a89271e60dfec8
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-05-05 19:22:09 +0200

    chore: fix spec for lap

--- src/app/entities/laps/lap.spec.ts
+++ src/app/entities/laps/lap.spec.ts
@@ -1,5 +1,6 @@
 import {LapInterface} from './lap.interface';
 import {Lap} from './lap';
+import {LapTypes} from './lap.types';
 
 describe('Lap', () => {
 
@@ -7,7 +8,7 @@ describe('Lap', () => {
 
   beforeEach(() => {
     lap = new Lap(new Date(0), new Date(100));
-    lap.type = 'Auto';
+    lap.type = LapTypes.AutoLap;
     lap.setID('123')
   });
 
@@ -18,7 +19,7 @@ describe('Lap', () => {
       'id': '123',
       'startDate': '1970-01-01T00:00:00.000Z',
       'endDate': '1970-01-01T00:00:00.100Z',
-      'type': 'Auto',
+      'type': 'Auto lap',
       'stats': [],
     });
 
