641ccd6eff75a08caed4e3616ea0340f34abd4e3
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-05-17 22:20:42 +0200

    chore:

--- src/app/services/color/app.device.colors.ts
+++ src/app/services/color/app.device.colors.ts
@@ -1,10 +1,19 @@
 import {AppColors} from './app.colors';
 
 export enum AppDeviceColors {
+  'Suunto 0' = AppColors.Green,
+  'Suunto 1' = AppColors.Green,
+  'Suunto 2' = AppColors.Green,
+  'Suunto 3' = AppColors.Green,
+  'Suunto 4' = AppColors.Green,
+  'Suunto 5' = AppColors.Green,
+  'Suunto 6' = AppColors.Green,
+  'Suunto 7' = AppColors.Green,
+  'Suunto 8' = AppColors.Green,
+  'Suunto 9' = AppColors.Green,
   'Suunto Ambit3 Sport' = AppColors.Orange,
   'Suunto Ambit3 Peak' = AppColors.Orange,
   'Suunto Spartan Ultra' = AppColors.Blue,
-  'Suunto 9' = AppColors.Green,
   'Suunto 3 Fitness' = AppColors.Pink,
   'Suunto Spartan Trainer' = AppColors.Purple,
   'Suunto Spartan WHR Baro' = AppColors.Red,
