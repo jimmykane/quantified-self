6ef86d61643084577d08abe9e2a7b27194c26388
Dimtrios Kanellopoulos <jimmykane9@gmail.com>
2018-05-24 17:21:23 +0200

    chore: anonymize IP

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ if (environment.production) {
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     ga('create', 'UA-61188889-2', 'auto');
+    ga('set', 'anonymizeIp', true);
     ga('send', 'pageview');`;
   document.head.appendChild(analyticsScript);
 
