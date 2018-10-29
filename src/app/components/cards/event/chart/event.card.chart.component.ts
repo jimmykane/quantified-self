e38e5e6ce0ede82e22d034bdfcf19a1337487dcb
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2018-10-29 21:30:00 +0200

    Revert "fix(chart): do not trigger twice the chart creation"
    This reverts commit e4224b29a471e3a17a5eff196171462099c6fed2.

--- src/app/components/cards/event/chart/event.card.chart.component.ts
+++ src/app/components/cards/event/chart/event.card.chart.component.ts
@@ -54,7 +54,9 @@ export class EventCardChartComponent implements OnChanges, OnInit, OnDestroy, Af
       return;
     }
     // If data changed... update
-    if (simpleChanges.selectedActivities || simpleChanges.event || (simpleChanges.useDistanceAxis && simpleChanges.useDistanceAxis.currentValue !== this.useDistanceAxis )) {
+    if (simpleChanges.selectedActivities || simpleChanges.event || simpleChanges.useDistanceAxis) {
+      // debugger
+      // @todo should not get triggered twice like atm
       this.destroyChart(); // Destroy to recreate (no update as its messy atm)
       this.chartData = this.getAllData();
     }
