766a1ed4056fb762bbeb636787725d8ac50cf09b
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2018-12-11 15:24:18 +0100

    chore: add firebase cred for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBdR4jbTKmm_P4L7t26IFAgFn6Eoo02aU0",
+    authDomain: "quantified-self-io.firebaseapp.com",
+    databaseURL: "https://quantified-self-io.firebaseio.com",
+    projectId: "quantified-self-io",
+    storageBucket: "quantified-self-io.appspot.com",
+    messagingSenderId: "242713487388"
+  }
 };
