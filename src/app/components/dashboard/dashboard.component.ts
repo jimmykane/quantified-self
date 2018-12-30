5dbb49eec5ab2f412b7a0a477c3f199c83637d24
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2018-12-30 17:52:28 +0100

    chore(dashboard): redirect if user is logged out

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,8 @@ import {combineLatest, Subscription} from 'rxjs';
 import {EventInterface} from 'quantified-self-lib/lib/events/event.interface';
 import {AppAuthService, AppUser} from '../../authentication/app.auth.service';
 import {switchMap} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,8 +21,14 @@ export class DashboardComponent implements OnInit, OnDestroy, OnChanges {
   events: EventInterface[];
   eventsSubscription: Subscription;
 
-  constructor(private authService: AppAuthService, private eventService: EventService) {
+  constructor(private router: Router,private authService: AppAuthService, private eventService: EventService, private snackBar: MatSnackBar,) {
     this.eventsSubscription = this.authService.user.pipe(switchMap((user) => {
+      if (!user){
+        this.router.navigate(['home']).then(() => {
+          this.snackBar.open('Logged out')
+        });
+        return;
+      }
       this.user = user;
       return this.eventService.getEventsForUser(user);
     })).subscribe((eventsArray) => {
