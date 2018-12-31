e112abf7121d923c077335d56c824d648759d400
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2018-12-31 22:48:28 +0100

    chore: move away from inhritance

--- src/app/components/user.abstract.component.ts
+++ src/app/components/user.abstract.component.ts
@@ -3,20 +3,8 @@ import {Subscription} from 'rxjs';
 import {AppAuthService} from '../authentication/app.auth.service';
 import {User} from 'quantified-self-lib/lib/users/user';
 
-export abstract class UserAbstractComponent implements OnInit, OnDestroy {
+export abstract class UserAbstractComponent {
 
    @Inject(AppAuthService) private authService: AppAuthService;
 
-   private userSubscription: Subscription;
-   public user: User;
-
-   ngOnInit(): void {
-    this.userSubscription = this.authService.user.subscribe((user) => {
-      this.user = user;
-    });
-  }
-
-  ngOnDestroy(): void {
-     this.userSubscription.unsubscribe();
-  }
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,21 +1,23 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UserAbstractComponent} from '../user.abstract.component';
 import {Privacy} from 'quantified-self-lib/lib/privacy/privacy.class.interface';
 import {User} from 'quantified-self-lib/lib/users/user';
+import {Subscription} from 'rxjs';
+import {AppAuthService} from '../../authentication/app.auth.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
 })
-export class UserComponent extends UserAbstractComponent {
+export class UserComponent implements OnInit, OnDestroy{
 
   public userFromParams: User;
   public user: User;
+ private userSubscription: Subscription;
 
-  constructor(private route: ActivatedRoute){
-    super();
+  constructor(private authService: AppAuthService, private route: ActivatedRoute){
   }
 
   ngOnInit(): void {
@@ -24,9 +26,16 @@ export class UserComponent extends UserAbstractComponent {
       this.userFromParams = {privacy: Privacy.private, uid: userID};
     }
 
+    this.userSubscription = this.authService.user.subscribe((user) => {
+      this.user = user;
+    });
   }
 
   isOwner() {
     return !!(this.userFromParams && this.user && (this.userFromParams.uid === this.user.uid));
+  }
+
+  ngOnDestroy(): void {
+     this.userSubscription.unsubscribe();
   }
 }
