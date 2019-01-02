de7a75c845f5f5ac0b57f767abb57dee76fa6683
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-01-02 17:17:50 +0100

    chore(sharing-service): add method for user profile share url

--- src/app/services/app.sharing.service.ts
+++ src/app/services/app.sharing.service.ts
@@ -6,4 +6,8 @@ export class SharingService {
   public getShareURLForEvent(userID: string, eventID: string): string {
     return `${window.location.protocol}//${window.location.host}/user/${userID}/event/${eventID}`;
   }
+
+  public getShareURLForUser(userID: string): string {
+    return `${window.location.protocol}//${window.location.host}/user/${userID}`;
+  }
 }
