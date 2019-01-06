8ed48b6d73cf5bc6e6024745e52983ae93dee621
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-01-06 19:29:18 +0100

    chore(user): add displayname and photourl defaults

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -43,6 +43,13 @@ export class UserComponent implements OnInit, OnDestroy {
     })).pipe(catchError((error) => {
       return of(null);
     })).subscribe((targetUser) => {
+      // Populate placeholders for display name etc
+      if (!targetUser.displayName){
+        targetUser.displayName = 'Anonymous';
+      }
+      if(!targetUser.photoURL){
+        targetUser.photoURL = `https://ui-avatars.com/api/?name=${targetUser.displayName}`
+      }
       if (!targetUser) {
         this.router.navigate(['home']).then(() => {
           this.snackBar.open('Not found...', null, {
