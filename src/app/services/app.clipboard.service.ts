117c4bce2279d498f0c6b52599c737359febbb81
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-01-08 19:57:23 +0100

    chore(clipboard-service): run outside Angular to copy

--- src/app/services/app.clipboard.service.ts
+++ src/app/services/app.clipboard.service.ts
@@ -1,14 +1,30 @@
-import {Injectable} from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
+import * as Raven from "raven-js";
+import {Log} from "ng2-logger/browser";
 
 
 @Injectable()
 export class ClipboardService {
+  private logger = Log.create('ClipboardService');
+
+  constructor(private zone: NgZone) {
+  }
+
   public copyToClipboard(text: string) {
-    const el = document.createElement('textarea');
-    el.value = text;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
+    try {
+      this.zone.runOutsideAngular(() => {
+        const el = document.createElement('textarea');
+        el.value = text;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+      });
+    } catch (e) {
+      this.logger.error(`Could not copy ${text}`);
+      // Log to Sentry
+      Raven.captureException(e);
+    }
+
   }
 }
