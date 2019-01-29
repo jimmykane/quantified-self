f2e59967ee17635cab5e1581128eb6653f59621d
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-01-29 08:29:36 +0100

    chore: meta and styles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 declare function require(moduleName: string): any;
 const { version: appVersion } = require('../../../../package.json');
@@ -11,4 +11,9 @@ const { version: appVersion } = require('../../../../package.json');
 })
 export class HomeComponent {
   public appVersion = appVersion;
+
+  @HostListener('window:resize', ['$event'])
+  getColumnsToDisplayDependingOnScreenSize(event?) {
+    return window.innerWidth < 600 ? 1 : 2;
+  }
 }
