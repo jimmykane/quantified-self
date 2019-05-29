f99cd3e43a525622f9591f43f59e6d624ed299b8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-05-29 15:37:51 +0200

    chore: add suunto logo as part of mat icons and use it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,14 @@ import {
 } from '@angular/core';
 import {ActionButtonService} from './services/action-buttons/app.action-button.service';
 import {ActionButton} from './services/action-buttons/app.action-button';
-import {MatSidenav, MatSnackBar} from '@angular/material';
+import {MatIconRegistry, MatSidenav, MatSnackBar} from '@angular/material';
 import {Subscription} from 'rxjs';
 import {Router, RoutesRecognized} from '@angular/router';
 import {filter, map} from 'rxjs/operators';
 import {AppAuthService} from './authentication/app.auth.service';
-import {SideNavService} from "./services/side-nav/side-nav.service";
-import {AppThemes} from "quantified-self-lib/lib/users/user.app.settings.interface";
+import {SideNavService} from './services/side-nav/side-nav.service';
+import {AppThemes} from 'quantified-self-lib/lib/users/user.app.settings.interface';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -38,8 +39,14 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy, AfterView
     private changeDetectorRef: ChangeDetectorRef,
     private actionButtonService: ActionButtonService,
     private sideNavService: SideNavService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
     private snackBar: MatSnackBar) {
 
+    this.matIconRegistry.addSvgIcon(
+      'suunto',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/suunto_logo.svg')
+    );
   }
 
   ngOnInit() {
@@ -59,7 +66,7 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy, AfterView
     }, 'material'));
 
     this.authService.user.subscribe((user) => {
-      if (!user){
+      if (!user) {
         return;
       }
       user.settings.appSettings.theme === AppThemes.Normal ? document.body.classList.remove('dark-theme') : document.body.classList.add('dark-theme');
