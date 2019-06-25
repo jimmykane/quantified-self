3efaded04c9fc3f6a15773d99a7388866807047c
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-06-25 22:21:34 +0200

    chore: migration done

--- functions/src/migrations.ts
+++ functions/src/migrations.ts
@@ -93,34 +93,34 @@ import * as admin from "firebase-admin";
 //   }
 // });
 
-
-export const migrateMerges = functions.region('europe-west2').runWith({timeoutSeconds: 180}).pubsub.schedule('every 2 hours').onRun(async (context) => {
-  // return;
-  // @todo delete indexes
-  const querySnapshot = await admin.firestore()
-    // .collectionGroup(`events`)
-    .collection('users').doc('M8gxUABg0UXQyVlFyhNMBsvl8bm1')
-    .collection('events')
-    .orderBy("name").where("name", '>=',  'Merged at')
-    // .limit()
-    .get();
-  console.log(`Found ${querySnapshot.size} to process`);
-  let count = 0;
-  const batch = admin.firestore().batch();
-  for (const doc of querySnapshot.docs) {
-    batch.update(doc.ref, {
-      name: 'Merged Event',
-      isMerge: true
-    });
-    // await getTokenData(eventDoc, true);
-    // console.log(`Parsed ${doc.id} for ${doc.ref.parent.parent.id} and ${doc.ref.parent.parent.parent.id}  ${doc.ref.parent.parent.parent.parent.id}`);
-
-    count++;
-  }
-  try {
-    await batch.commit();
-  } catch (e) {
-    console.log(e)
-  }
-  console.log(`Parsed ${count} docs out of ${querySnapshot.size} and a total of writes`);
-});
+//
+// export const migrateMerges = functions.region('europe-west2').runWith({timeoutSeconds: 180}).pubsub.schedule('every 2 hours').onRun(async (context) => {
+//   // return;
+//   // @todo delete indexes
+//   const querySnapshot = await admin.firestore()
+//     .collectionGroup(`events`)
+//     // .collection('users').doc('M8gxUABg0UXQyVlFyhNMBsvl8bm1')
+//     // .collection('events')
+//     .orderBy("name").where("name", '>=',  'Merged at')
+//     .limit(400)
+//     .get();
+//   console.log(`Found ${querySnapshot.size} to process`);
+//   let count = 0;
+//   const batch = admin.firestore().batch();
+//   for (const doc of querySnapshot.docs) {
+//     batch.update(doc.ref, {
+//       name: 'Merged Event',
+//       isMerge: true
+//     });
+//     // await getTokenData(eventDoc, true);
+//     // console.log(`Parsed ${doc.id} for ${doc.ref.parent.parent.id} and ${doc.ref.parent.parent.parent.id}  ${doc.ref.parent.parent.parent.parent.id}`);
+//
+//     count++;
+//   }
+//   try {
+//     await batch.commit();
+//   } catch (e) {
+//     console.log(e)
+//   }
+//   console.log(`Parsed ${count} docs out of ${querySnapshot.size} and a total of writes`);
+// });
