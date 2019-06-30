abed15b2da2ad19f9a36ab2d72fe5025099af638
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-06-30 22:30:51 +0200

    chore: move down

--- src/app/components/charts/chart.abstract.ts
+++ src/app/components/charts/chart.abstract.ts
@@ -117,7 +117,7 @@ export abstract class ChartAbstract implements OnDestroy {
   protected getExportingMenu(): am4core.ExportMenu {
     const exportingMenu = new am4core.ExportMenu();
     exportingMenu.align = 'right';
-    exportingMenu.verticalAlign = 'middle';
+    exportingMenu.verticalAlign = 'bottom';
     exportingMenu.items = [{
       label: '...️',
       menu: [
