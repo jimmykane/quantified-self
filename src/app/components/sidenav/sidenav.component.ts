e97567acecf078b28c7b4716ed11d108140ebac5
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-07-03 22:34:07 +0200

    chore: minor fixes and packaging

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,11 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from '../../services/app.event.service';
-import {ActivatedRoute, Params, Router} from '@angular/router';
-import {Subscription} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
 import {EventInterface} from 'quantified-self-lib/lib/events/event.interface';
 import {AppAuthService} from '../../authentication/app.auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {SideNavService} from "../../services/side-nav/side-nav.service";
+import {SideNavService} from '../../services/side-nav/side-nav.service';
 
 declare function require(moduleName: string): any;
 const { version: appVersion } = require('../../../../package.json');
