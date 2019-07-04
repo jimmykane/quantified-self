ef448d6c01867c1af877a4780554c91b89b15d65
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-07-04 19:45:38 +0200

    fix: import

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -1,4 +1,4 @@
-import base58 = require("bs58");
+import * as base58 from 'bs58';
 
 export function generateIDFromParts(parts: string[]): string{
   return base58.encode(Buffer.from(`${parts.join(':')}`));
