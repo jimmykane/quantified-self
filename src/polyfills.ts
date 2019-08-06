835a7162a99df69b180564a3723284820baf6694
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-08-06 22:05:41 +0200

    chore: bump versions

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -66,3 +66,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+
+// Buffer that is a dependency from Fit parser uses global to check against typed arrays
+(window as any).global = window;
