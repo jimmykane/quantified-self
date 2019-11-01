fe32f49ab329d72a00cfaccd4fca6fe354059fc6
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-11-01 18:51:11 +0100

    chore: finetune animations bar

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -18,11 +18,11 @@ export const slideInAnimation =
       group([
         query(':enter', [
           style({transform: 'translateX(100%)'}),
-          animate('0.5s ease-in-out', style({transform: 'translateX(0%)'}))
+          animate('0.3s ease-in-out', style({transform: 'translateX(0%)'}))
         ], {optional: true}),
         query(':leave', [
           style({transform: 'translateX(0%)'}),
-          animate('0.5s ease-in-out', style({transform: 'translateX(-100%)'}))
+          animate('0.3s ease-in-out', style({transform: 'translateX(-100%)'}))
         ], {optional: true}),
       ])
     ]),
