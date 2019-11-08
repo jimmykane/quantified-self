68bd1c4f0a9679bc4c8846cd0a672e26548ccf86
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2019-11-08 21:27:40 +0100

    chore: minor fix

--- src/app/components/charts/timeline/charts.timeline.component.ts
+++ src/app/components/charts/timeline/charts.timeline.component.ts
@@ -48,7 +48,7 @@ export class ChartsTimelineComponent extends DashboardChartAbstract implements O
     chart.yAxisRadius = am4core.percent(100);
     chart.innerRadius = am4core.percent(45);
     chart.paddingTop = -15;
-    chart.paddingBottom = -60;
+    chart.paddingBottom = -55;
 
     const categoryAxis = chart.yAxes.push(<am4charts.Axis<am4plugins_timeline.AxisRendererCurveY>>this.getCategoryAxis(this.chartDataCategoryType, this.chartDataDateRange));
     // categoryAxis.dataFields.category = 'time';
