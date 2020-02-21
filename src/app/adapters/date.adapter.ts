472801100a7af65b775c518bf59081abdbf896b7
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-02-21 19:42:40 +0100

    chore: fixes

--- src/app/adapters/date.adapter.ts
+++ src/app/adapters/date.adapter.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
-import { Platform } from "@angular/cdk/platform";
 
 @Injectable()
 export class MondayDateAdapter extends NativeDateAdapter {
