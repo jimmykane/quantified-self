c1689dd8bc9ff90e14f45538075359f400764cb8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-02-25 22:02:11 +0100

    fix: non existent

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -4,7 +4,6 @@ import {SharedModule} from './shared.module';
 import {CommonModule} from '@angular/common';
 import { HomeRoutingModule } from '../home-routing.module';
 import { HomeComponent } from '../components/home/home.component';
-import { HomeLiveChartComponent } from '../components/home/live-chart/home.live-chart.component';
 
 @NgModule({
   imports: [
@@ -16,7 +15,6 @@ import { HomeLiveChartComponent } from '../components/home/live-chart/home.live-
   exports: [],
   declarations: [
     HomeComponent,
-    HomeLiveChartComponent
   ],
   entryComponents: [],
 })
