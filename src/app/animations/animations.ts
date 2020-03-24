17aa10a629211bff5c84e143d7d701674a24408a
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-03-24 09:44:18 +0100

    chore: animations

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -5,8 +5,8 @@ export const rowsAnimation =
     transition('void => *', [
       style({height: '*', opacity: '0', transform: 'translateY(-100%)', 'box-shadow': 'none'}),
       sequence([
-        animate('.50s ease', style({height: '*', opacity: '.4', transform: 'translateY(0)', 'box-shadow': 'none'})),
-        animate('.50s ease', style({height: '*', opacity: 1, transform: 'translateY(0)'}))
+        animate('.65s ease', style({height: '*', opacity: '.4', transform: 'translateY(0)', 'box-shadow': 'none'})),
+        animate('.65s ease', style({height: '*', opacity: 1, transform: 'translateY(0)'}))
       ])
     ])
   ]);
@@ -18,11 +18,11 @@ export const slideInAnimation =
       group([
         query(':enter', [
           style({opacity: 0, transform: 'translateX(100%)'}),
-          animate('0.6s ease-in', style({ opacity: 1, transform: 'translateX(0%)'}))
+          animate('0.65s ease', style({ opacity: 1, transform: 'translateX(0%)'}))
         ], {optional: true}),
         query(':leave', [
           style({opacity: 1, transform: 'translateX(0%)'}),
-          animate('0.6s ease-in', style({opacity: 0, transform: 'translateX(-100%)'}))
+          animate('0.65s ease', style({opacity: 0, transform: 'translateX(-100%)'}))
         ], {optional: true}),
       ])
     ]),
