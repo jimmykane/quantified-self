7a808d2042e6973af3dda93e86f18a558a5897f8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-04-03 10:01:57 +0200

    chore: improve service worker

--- src/app/services/app.update.service.ts
+++ src/app/services/app.update.service.ts
@@ -1,19 +1,22 @@
-import {Injectable} from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { interval } from 'rxjs';
+import { concat, interval } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppUpdateService {
-  constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
-    if (swUpdate.isEnabled) {
-      interval(5 * 60 * 1000).subscribe(() => swUpdate.checkForUpdate()
-        .then(() => console.log('checking for updates')));
-    }
-    this.swUpdate.available.subscribe(evt => {
+  constructor(appRef: ApplicationRef, updates: SwUpdate, private snackbar: MatSnackBar) {
+    // Allow the app to stabilize first, before starting polling for updates with `interval()`.
+    const appIsStable = appRef.isStable.pipe(first(isStable => isStable === true));
+    const everySixMinutes = interval(6 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable, everySixMinutes);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+    updates.available.subscribe(evt => {
       const snack = this.snackbar.open('There is a new version available', 'Reload');
       snack
         .onAction()
