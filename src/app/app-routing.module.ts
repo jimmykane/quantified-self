bf67dfe24bec8bf3a1a6f3db8dbf96a2c87a5f75
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-04-11 19:37:12 +0200

    chore: enable view restoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule],
 })
 
