89526410dc1604c25d0419ecba1ae2c9efa81af8
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-10-09 15:32:12 +0200

    chore: progress

--- src/app/components/heatmap/heatmap.component.ts
+++ src/app/components/heatmap/heatmap.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
@@ -12,6 +11,7 @@ import { AppAuthService } from '../../authentication/app.auth.service';
 import { Router } from '@angular/router';
 import * as L from 'leaflet';
 import 'leaflet-providers';
+import 'leaflet-easybutton';
 import { AppEventService } from '../../services/app.event.service';
 import { take } from 'rxjs/operators';
 import { Log } from 'ng2-logger/browser';
@@ -35,7 +35,7 @@ import WhereFilterOp = firebase.firestore.WhereFilterOp;
   styleUrls: ['./heatmap.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeatmapComponent extends LoadingAbstractDirective implements AfterViewInit, OnInit {
+export class HeatmapComponent extends LoadingAbstractDirective implements OnInit {
   @ViewChild('mapDiv', {static: true}) mapDiv: ElementRef;
   public dataSubscription: Subscription;
   uploadPercent: Observable<number>;
@@ -44,6 +44,8 @@ export class HeatmapComponent extends LoadingAbstractDirective implements AfterV
   private map: L.Map;
   private user: User;
   private positions: any[] = [];
+  private viewAllButton: L.Control.EasyButton;
+  private scrolled = false;
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
@@ -59,6 +61,9 @@ export class HeatmapComponent extends LoadingAbstractDirective implements AfterV
 
   async ngOnInit() {
     this.loading()
+    this.initMap()
+    this.getStartingLocation();
+
     this.user = await this.authService.user.pipe(take(1)).toPromise();
     const dates = getDatesForDateRange(DateRanges.thisYear, DaysOfTheWeek.Monday);
     const where = []
@@ -72,47 +77,42 @@ export class HeatmapComponent extends LoadingAbstractDirective implements AfterV
       opStr: <WhereFilterOp>'<=', // Should remove mins from date
       value: dates.endDate.getTime()
     });
-    this.dataSubscription = await this.eventService.getEventsBy(this.user, where, 'startDate', null, 500).subscribe(async (events) => {
-      events = events.filter((event) => event.getStat(DataStartPosition.type));
-      if (!events || !events.length) {
-        this.loaded() // @todo fix add no data
-        return;
-      }
-      for (const event of events) {
-        this.eventService.getEventActivitiesAndSomeStreams(this.user,
-          event.getID(),
-          [DataLatitudeDegrees.type, DataLongitudeDegrees.type])
-          .pipe(take(1)).toPromise().then((fullEvent) => {
-            this.logger.info(`Promise completed`)
-            const lineOptions = Object.assign({}, DEFAULT_OPTIONS.lineOptions);
-            fullEvent.getActivities()
-              .filter((activity) => activity.hasPositionData())
-              .forEach((activity) => {
-                const positionalData = activity.getPositionData().filter((position) => position).map((position) => {
-                  return {
-                    lat: position.latitudeDegrees,
-                    lng: position.longitudeDegrees
-                  }
-                });
-                lineOptions.color = this.eventColorService.getColorForActivityTypeByActivityTypeGroup(activity.type)
-                const line = L.polyline(positionalData, lineOptions);
-                this.positions.push({event: fullEvent, line: line})
-                line.addTo(this.map);
-                if (this.isLoading) {
-                  this.loaded()
-                  this.center(this.positions.map((p) => p.line))
+    let events = await this.eventService.getEventsBy(this.user, where, 'startDate', null, 100).pipe(take(1)).toPromise()
+    events = events.filter((event) => event.getStat(DataStartPosition.type));
+    if (!events || !events.length) {
+      // this.loaded() // @todo fix add no data
+      return;
+    }
+    for (const event of events) {
+      this.eventService.getEventActivitiesAndSomeStreams(this.user,
+        event.getID(),
+        [DataLatitudeDegrees.type, DataLongitudeDegrees.type])
+        .pipe(take(1)).toPromise().then((fullEvent) => {
+          this.logger.info(`Promise completed`)
+          const lineOptions = Object.assign({}, DEFAULT_OPTIONS.lineOptions);
+          fullEvent.getActivities()
+            .filter((activity) => activity.hasPositionData())
+            .forEach((activity) => {
+              const positionalData = activity.getPositionData().filter((position) => position).map((position) => {
+                return {
+                  lat: position.latitudeDegrees,
+                  lng: position.longitudeDegrees
                 }
-              })
-          })
-      }
-    })
+              });
+              lineOptions.color = this.eventColorService.getColorForActivityTypeByActivityTypeGroup(activity.type)
+              const line = L.polyline(positionalData, lineOptions);
+              this.positions.push({event: fullEvent, line: line})
+              line.addTo(this.map);
+              if (this.isLoading) {
+                this.loaded()
+                this.center(this.positions.map((p) => p.line))
+              }
+            })
+      })
+    }
   }
 
-  ngAfterViewInit(): void {
-    this.initMap()
-  }
-
-  center(lines = []) {
+  center(lines) {
     this.zone.runOutsideAngular(() => {
       this.map.fitBounds((L.featureGroup(lines)).getBounds(), {
         noMoveStart: false,
@@ -121,22 +121,58 @@ export class HeatmapComponent extends LoadingAbstractDirective implements AfterV
       });
 
     })
-    // this.clearScroll();
-    // this.map.addEventListener('movestart');
+    if (!this.scrolled) {
+      this.clearScroll();
+    }
+  }
+
+  getStartingLocation() {
+    navigator.geolocation.getCurrentPosition(pos => {
+      if (!this.scrolled && this.positions.length === 0) {
+        this.map.panTo([pos.coords.latitude, pos.coords.longitude], {
+          noMoveStart: true,
+          animate: false,
+        });
+        // noMoveStart doesn't seem to have an effect, see Leaflet
+        // issue: https://github.com/Leaflet/Leaflet/issues/5396
+        this.clearScroll();
+      }
+    });
+  }
+
+  private markScrolled() {
+    this.map.removeEventListener('movestart', this.markScrolled);
+    this.scrolled = true;
+  }
+
+  private clearScroll() {
+    this.scrolled = false;
+    this.map.addEventListener('movestart', this.markScrolled)
   }
 
   private initMap(): void {
     this.map = this.zone.runOutsideAngular(() => {
       const map = L.map(this.mapDiv.nativeElement, {
-        // center: [39.8282, -98.5795],
+        center: [0, 0],
         fadeAnimation: false,
         zoomAnimation: true,
-        zoom: 10,
-        preferCanvas: true,
+        zoom: 2,
+        preferCanvas: false,
         dragging: !L.Browser.mobile
       });
       const tiles = L.tileLayer.provider(AVAILABLE_THEMES[0])
       tiles.addTo(map);
+      this.viewAllButton = L.easyButton({
+        type: 'animate',
+        states: [{
+          icon: 'stats',
+          stateName: 'default',
+          title: 'Zoom to all tracks',
+          onClick: () => {
+            this.center(this.positions.map((p) => p.line));
+          },
+        }],
+      }).addTo(map);
       return map
     })
   }
