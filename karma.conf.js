306ab11a12434ee5c6813612657f1f1d2250baf5
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-10-10 18:04:27 +0200

    chore: do not fail on empty test suite

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false
+    singleRun: false,
+    failOnEmptyTestSuite: false
   });
 };
