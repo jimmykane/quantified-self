45a9f6620d87bbe7f9c820b35c5cd10c0de98640
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2020-11-19 19:39:48 +0100

    chore: do not iterate over modified

--- functions/src/history.ts
+++ functions/src/history.ts
@@ -141,7 +141,7 @@ export async function getWorkoutQueueItems(serviceName: ServiceNames, serviceTok
           'Ocp-Apim-Subscription-Key': functions.config().suuntoapp.subscription_key,
           json: true,
         },
-        url: `https://cloudapi.suunto.com/v2/workouts?since=${startDate.getTime()}&until=${endDate.getTime()}&limit=1000000`,
+        url: `https://cloudapi.suunto.com/v2/workouts?since=${startDate.getTime()}&until=${endDate.getTime()}&limit=1000000&filter-by-modification-time=false`,
       });
       result = JSON.parse(result)
       if (result.error) {
@@ -149,7 +149,7 @@ export async function getWorkoutQueueItems(serviceName: ServiceNames, serviceTok
       }
       return result.payload
         .filter((item: any) => (new Date(item.startTime)) >= startDate && (new Date(item.startTime)) <= endDate)
-        .filter((item: any) => item.workoutKey)
+        .filter((item: any) => !!item.workoutKey)
         .map((item: any) => {
         return {
           id: generateIDFromParts([serviceToken.userName, item.workoutKey]),
