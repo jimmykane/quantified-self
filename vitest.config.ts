fa1e98de6f4e2a9304b88d4a3c0334cfff26a9b4
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2025-12-09 19:22:36 +0200

    chore: fix for vite

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+    plugins: [angular()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['src/test-setup.ts'],
+        include: ['**/*.spec.ts'],
+        reporters: ['default'],
+    },
+});
