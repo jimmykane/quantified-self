60ed8895d238b274e2b051ae51ccf021288bae98
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2025-12-10 08:54:16 +0200

    fix: environments

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -10,6 +10,7 @@ export const environment = {
   production: false,
   beta: true,
   localhost: false,
+  useAuthEmulator: false,
   firebase: {
     apiKey: 'AIzaSyBdR4jbTKmm_P4L7t26IFAgFn6Eoo02aU0',
     authDomain: 'beta.quantified-self.io',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   production: true,
   beta: false,
   localhost: false,
+  useAuthEmulator: false,
   firebase: {
     apiKey: 'AIzaSyBdR4jbTKmm_P4L7t26IFAgFn6Eoo02aU0',
     authDomain: 'quantified-self.io',
