e2d2e32f2a7fb63cc5f5a83baedfb3356450b3f0
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2025-12-10 12:48:21 +0200

    Revert "chore: diagnose"
    This reverts commit 8b25568eda5455dc22f49f6fef6d074b109e6ead.

--- functions/src/diagnose.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Diagnostic Test', () => {
-    it('should pass if the environment is working', () => {
-        expect(true).toBe(true);
-        console.log('Diagnostic test running successfully');
-    });
-});
