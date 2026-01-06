86587c17b0c33b1451e936cf1281cc0988f92dc9
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-01-06 13:22:01 +0200

    chore: Configure APP_STORAGE providers for browser and server

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,6 +3,8 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { APP_STORAGE } from './services/storage/app.storage.token';
+import { MemoryStorage } from './services/storage/memory.storage';
 
 @NgModule({
     imports: [
@@ -10,5 +12,8 @@ import { AppComponent } from './app.component';
         ServerModule,
     ],
     bootstrap: [AppComponent],
+    providers: [
+        { provide: APP_STORAGE, useClass: MemoryStorage }
+    ]
 })
 export class AppServerModule { }
