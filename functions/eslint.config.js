7c102c38ac12e903b84432d8688c45e90441cde5
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-01-06 12:18:50 +0200

    chore: add back eslint

---
+++ functions/eslint.config.js
@@ -0,0 +1,21 @@
+// eslint.config.js
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["src/**/*.{ts,js}"],
+        rules: {
+            "no-unused-vars": "off", // Handled by TS
+            "no-undef": "off",      // Handled by TS
+            "@typescript-eslint/no-var-requires": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "warn"
+        }
+    },
+    {
+        ignores: ["node_modules/", "lib/", "coverage/"]
+    }
+);
