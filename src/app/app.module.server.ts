5f4d023a4ede7194dc8d28e2e7b48764bdc0343c
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-01-06 13:42:43 +0200

    feat(ssr): Provide NoopIconRegistry in AppServerModule

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { APP_STORAGE } from './services/storage/app.storage.token';
 import { MemoryStorage } from './services/storage/memory.storage';
+import { NoopIconRegistry } from './services/storage/noop.icon.registry';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @NgModule({
     imports: [
@@ -13,7 +15,8 @@ import { MemoryStorage } from './services/storage/memory.storage';
     ],
     bootstrap: [AppComponent],
     providers: [
-        { provide: APP_STORAGE, useClass: MemoryStorage }
+        { provide: APP_STORAGE, useClass: MemoryStorage },
+        { provide: MatIconRegistry, useClass: NoopIconRegistry }
     ]
 })
 export class AppServerModule { }
