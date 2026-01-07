94f6f3129b93636c202a4ead1efe7868f3380c66
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-01-07 21:47:45 +0200

    chore: revert file

--- src/app/services/app.remote-config.service.ts
+++ src/app/services/app.remote-config.service.ts
@@ -5,9 +5,6 @@ import { AppWindowService } from './app.window.service';
 import { AppUserService } from './app.user.service';
 import { environment } from '../../environments/environment';
 import { LoggerService } from './logger.service';
-import { APP_STORAGE } from './storage/app.storage.token';
-import { Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 
 /**
  * Remote Config Service - Bypasses Firebase SDK completely due to persistent bugs.
@@ -30,9 +27,7 @@ export class AppRemoteConfigService {
     constructor(
         private windowService: AppWindowService,
         private userService: AppUserService,
-        private logger: LoggerService,
-        @Inject(APP_STORAGE) private storage: Storage,
-        @Inject(PLATFORM_ID) private platformId: Object
+        private logger: LoggerService
     ) {
         // Check admin status initially
         this.checkAdminStatus();
@@ -160,7 +155,7 @@ export class AppRemoteConfigService {
 
     private getOrCreateInstanceId(): string {
         const key = 'rc_instance_id';
-        let id = this.storage.getItem(key);
+        let id = localStorage.getItem(key);
         if (!id) {
             // crypto.randomUUID() is only available in secure contexts (HTTPS/localhost)
             if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
@@ -173,24 +168,21 @@ export class AppRemoteConfigService {
                     return v.toString(16);
                 });
             }
-            this.storage.setItem(key, id);
+            localStorage.setItem(key, id);
         }
         return id;
     }
 
     private isBypassEnabled(): boolean {
         const STORAGE_KEY = 'bypass_maintenance';
+        const hasQueryParam = this.windowService.windowRef.location.search.includes('bypass_maintenance=true');
 
-        if (isPlatformBrowser(this.platformId)) {
-            const hasQueryParam = this.windowService.windowRef.location.search.includes('bypass_maintenance=true');
-
-            if (hasQueryParam) {
-                this.storage.setItem(STORAGE_KEY, 'true');
-                return true;
-            }
+        if (hasQueryParam) {
+            localStorage.setItem(STORAGE_KEY, 'true');
+            return true;
         }
 
-        return this.storage.getItem(STORAGE_KEY) === 'true';
+        return localStorage.getItem(STORAGE_KEY) === 'true';
     }
 
     getMaintenanceMode(): Observable<boolean> {
