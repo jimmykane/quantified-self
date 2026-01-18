94f91a56778c631e0ed48fa7e68a5fcdba98cd05
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-01-18 18:47:41 +0200

    chore: disable app check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
 import { provideFirestore, initializeFirestore } from '@angular/fire/firestore';
 import { getApp } from '@angular/fire/app';
 import { provideFunctions, getFunctions, httpsCallable, connectFunctionsEmulator } from '@angular/fire/functions';
-import { provideAppCheck, initializeAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
+// import { provideAppCheck, initializeAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
 import { providePerformance, getPerformance } from '@angular/fire/performance';
 import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService, setAnalyticsCollectionEnabled } from '@angular/fire/analytics';
 import { provideRemoteConfig, getRemoteConfig } from '@angular/fire/remote-config';
@@ -78,15 +78,15 @@ import { APP_STORAGE } from './services/storage/app.storage.token';
     }),
     provideHttpClient(withInterceptorsFromDi()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAppCheck(() => {
-      const provider = new ReCaptchaV3Provider(environment.firebase.recaptchaSiteKey);
-      if (!environment.production && !environment.beta) {
-        (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
-      } else {
-        (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = false;
-      }
-      return initializeAppCheck(getApp(), { provider, isTokenAutoRefreshEnabled: true });
-    }),
+    // provideAppCheck(() => {
+    //   const provider = new ReCaptchaV3Provider(environment.firebase.recaptchaSiteKey);
+    //   if (!environment.production && !environment.beta) {
+    //     (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+    //   } else {
+    //     (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = false;
+    //   }
+    //   return initializeAppCheck(getApp(), { provider, isTokenAutoRefreshEnabled: true });
+    // }),
     provideAuth(() => {
       const auth = getAuth();
       if (environment.useAuthEmulator) {
