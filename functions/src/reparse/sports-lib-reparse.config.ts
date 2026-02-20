63c5570b9cf691c68b77650fe279efa227a1a818
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-02-20 14:33:54 +0200

    chore: enable task

--- functions/src/reparse/sports-lib-reparse.config.ts
+++ functions/src/reparse/sports-lib-reparse.config.ts
@@ -1,7 +1,7 @@
 export const SPORTS_LIB_REPARSE_TARGET_VERSION = '9.1.4';
 
 export const SPORTS_LIB_REPARSE_RUNTIME_DEFAULTS = {
-    enabled: false,
+    enabled: true,
     scanLimit: 200,
     enqueueLimit: 100,
     includeFreeUsers: false,
