45d4f6cf6cb03510019057bbdcb67095bac68292
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-02-25 13:33:08 +0200

    chore: increase the limits release ze krakenista

--- functions/src/reparse/sports-lib-reparse.config.ts
+++ functions/src/reparse/sports-lib-reparse.config.ts
@@ -2,7 +2,7 @@ export const SPORTS_LIB_REPARSE_TARGET_VERSION = '9.1.5';
 
 export const SPORTS_LIB_REPARSE_RUNTIME_DEFAULTS = {
     enabled: true,
-    scanLimit: 1000,
-    enqueueLimit: 800,
+    scanLimit: 5000,
+    enqueueLimit: 4000,
     uidAllowlist: [],
 } as const;
