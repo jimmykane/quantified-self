4eafccf92f98188159e632dc53933d05b2278660
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-02-25 14:05:54 +0200

    chore: decrease limits

--- functions/src/reparse/sports-lib-reparse.config.ts
+++ functions/src/reparse/sports-lib-reparse.config.ts
@@ -2,7 +2,7 @@ export const SPORTS_LIB_REPARSE_TARGET_VERSION = '9.1.5';
 
 export const SPORTS_LIB_REPARSE_RUNTIME_DEFAULTS = {
     enabled: true,
-    scanLimit: 5000,
-    enqueueLimit: 4000,
+    scanLimit: 1000,
+    enqueueLimit: 800,
     uidAllowlist: [],
 } as const;
