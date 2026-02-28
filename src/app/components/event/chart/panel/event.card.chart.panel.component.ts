6dd1ce392d237cd6a47302289880dfcc16fe60d0
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-02-28 18:01:12 +0200

    Revert "chore: filter filter"
    This reverts commit c38c3a3cf04035d1342a1ec5d87a8446f622c09f.

--- src/app/components/event/chart/panel/event.card.chart.panel.component.ts
+++ src/app/components/event/chart/panel/event.card.chart.panel.component.ts
@@ -334,7 +334,7 @@ export class EventCardChartPanelComponent implements AfterViewInit, OnChanges, O
         {
           type: 'inside',
           xAxisIndex: 0,
-          filterMode: 'filter',
+          filterMode: 'none',
           throttle: DATA_ZOOM_THROTTLE_MS,
           zoomOnMouseWheel: false,
           moveOnMouseMove: true,
@@ -347,7 +347,7 @@ export class EventCardChartPanelComponent implements AfterViewInit, OnChanges, O
           show: this.showZoomBar,
           height: 16,
           top: 8,
-          filterMode: 'filter',
+          filterMode: 'none',
           showDataShadow: false,
           throttle: DATA_ZOOM_THROTTLE_MS,
         }
@@ -606,7 +606,7 @@ export class EventCardChartPanelComponent implements AfterViewInit, OnChanges, O
           right: 12,
           top: 10,
           height: 20,
-          filterMode: 'filter',
+          filterMode: 'none',
           showDataShadow: false,
           showDetail: false,
           handleSize: 18,
