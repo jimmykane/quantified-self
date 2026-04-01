e7ba0c716e7f8e29c03a1b322b5b449a7634ddce
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-04-01 18:22:48 +0300

    chore: bump reparse version

--- functions/src/reparse/sports-lib-reparse.config.ts
+++ functions/src/reparse/sports-lib-reparse.config.ts
@@ -1,4 +1,4 @@
-export const SPORTS_LIB_REPARSE_TARGET_VERSION = '9.1.5';
+export const SPORTS_LIB_REPARSE_TARGET_VERSION = '10.0.0';
 
 export const SPORTS_LIB_REPARSE_RUNTIME_DEFAULTS = {
     enabled: true,
