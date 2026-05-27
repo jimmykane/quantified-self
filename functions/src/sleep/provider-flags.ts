9b5d22f5248d75fc67980cb78577a2b6117db84c
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2026-05-27 14:19:46 +0300

    chore: enable sleep sync for Garmin

--- functions/src/sleep/provider-flags.ts
+++ functions/src/sleep/provider-flags.ts
@@ -1,7 +1,6 @@
 import { SLEEP_PROVIDERS, SleepProvider } from '../../../shared/sleep';
 
 export const SLEEP_SYNC_DISABLED_PROVIDERS: readonly SleepProvider[] = [
-    SLEEP_PROVIDERS.GarminAPI,
     SLEEP_PROVIDERS.COROSAPI,
 ];
 
